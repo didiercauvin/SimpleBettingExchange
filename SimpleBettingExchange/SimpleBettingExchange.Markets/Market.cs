f7951fcea9168bcbc21ed9cd6f7bbc3b08fac9ab
didier.cauvin <didier.cauvin@mediane.tm.fr>
2024-11-27 16:30:22 +0100

    refacto

---
+++ SimpleBettingExchange/SimpleBettingExchange.Markets/Market.cs
@@ -0,0 +1,34 @@
+﻿namespace SimpleBettingExchange.Markets;
+
+public class Market
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public MarketStatus Status { get; set; }
+    public MarketLine[] Lines { get; set; }
+
+    private Market(Guid id, string name, IEnumerable<MarketLine> lines)
+    {
+        Id = id;
+        Name = name;
+        Status = MarketStatus.Created;
+        Lines = lines.ToArray();
+    }
+
+    public static Market Create(Guid id, string name, IEnumerable<MarketLine> lines)
+        => new Market(id, name, lines);
+}
+
+public enum MarketStatus { Created, Opened, Suspended, Closed }
+
+public class MarketLine
+{
+    public MarketLine(Guid id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+}
---
+++ SimpleBettingExchange/SimpleBettingExchange.Markets/MarketServices.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace SimpleBettingExchange.Markets;
+
+public static class MarketServices
+{
+    public static async Task Handle(CreateMarket createMarket, IMarketRepository repository, CancellationToken ct)
+    {
+        var market = Market.Create(createMarket.Id, createMarket.Name, createMarket.Lines.Select(l => new MarketLine(Guid.NewGuid(), l.Name)));
+
+        await repository.Add(market, ct);
+    }
+}
