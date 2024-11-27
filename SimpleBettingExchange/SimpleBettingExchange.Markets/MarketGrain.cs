1cfe5501d1e0c3b2929ad3367ca6f6151779eb64
didier.cauvin <didier.cauvin@mediane.tm.fr>
2024-11-27 14:17:36 +0100

    marketsatet

--- SimpleBettingExchange/SimpleBettingExchange.Markets/MarketGrain.cs
+++ SimpleBettingExchange/SimpleBettingExchange.Markets/MarketGrain.cs
@@ -74,36 +74,3 @@ public class MarketGrain : JournaledGrain<MarketState, IEvent>, IMarketGrain, IC
     }
 }
 
-public record MarketCreated(Guid Id, string Name, IEnumerable<MarketLineState> Lines, DateTimeOffset CreatedAt) : IEvent;
-
-public enum MarketStatus { Created, Opened, Suspended, Closed }
-
-public class MarketState
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public MarketStatus State { get; set; }
-    public MarketLineState[] Lines { get; set; }
-
-    public void When(IEvent @event)
-    {
-        switch (@event)
-        {
-            case MarketCreated created: Apply(created); break;
-        }
-    }
-
-    public void Apply(MarketCreated created)
-    {
-        Id = created.Id;
-        Name = created.Name;
-        State = MarketStatus.Created;
-        Lines = created.Lines.ToArray();
-    }
-}
-
-public class MarketLineState(Guid id, string name)
-{
-    public Guid Id { get; } = id;
-    public string Name { get; } = name;
-}
---
+++ SimpleBettingExchange/SimpleBettingExchange.Markets/MarketState.cs
@@ -0,0 +1,35 @@
+﻿namespace SimpleBettingExchange.Markets;
+
+public record MarketCreated(Guid Id, string Name, IEnumerable<MarketLineState> Lines, DateTimeOffset CreatedAt) : IEvent;
+
+public enum MarketStatus { Created, Opened, Suspended, Closed }
+
+public class MarketState
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public MarketStatus State { get; set; }
+    public MarketLineState[] Lines { get; set; }
+
+    public void When(IEvent @event)
+    {
+        switch (@event)
+        {
+            case MarketCreated created: Apply(created); break;
+        }
+    }
+
+    public void Apply(MarketCreated created)
+    {
+        Id = created.Id;
+        Name = created.Name;
+        State = MarketStatus.Created;
+        Lines = created.Lines.ToArray();
+    }
+}
+
+public class MarketLineState(Guid id, string name)
+{
+    public Guid Id { get; } = id;
+    public string Name { get; } = name;
+}
